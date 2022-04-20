79fec1acc73ec713606f3e1daaf20166272d719c
Dulaj <shad3s.of.m3@gmail.com>
2022-04-21 00:40:08 +0530

    add docker files

--- server/util/database.js
+++ server/util/database.js
@@ -1,6 +1,5 @@
 const { Sequelize } = require("sequelize");
 
-// Option 3: Passing parameters separately (other dialects)
 const sequelize = new Sequelize("time_tracker", "root", "password", {
   host: "localhost",
   dialect: "mysql",
