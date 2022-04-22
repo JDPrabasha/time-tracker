9dca90993b883d661294ead1e07d1968d04f72a2
Dulaj <shad3s.of.m3@gmail.com>
2022-04-22 17:48:09 +0530

    fix chartjs bug

--- client/src/containers/Insights/Insights.js
+++ client/src/containers/Insights/Insights.js
@@ -8,54 +8,52 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 function Insights() {
   const axios = require("axios");
-  const [data, setData] = useState([]);
-  const [chartData, setChartData] = useState({
-    labels: ["red", "blue", "green"],
-    datasets: [
-      {
-        label: "time spent",
-        data: [4, 6, 7, 7],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  });
 
-  useEffect(() => {
+  const [chartData, setChartData] = useState({});
+
+  const chart = () => {
     axios.get("http://localhost:3000/project/log").then((response) => {
-      setData(response.data);
-      const labels = data.map((project) => {
+      const labels = response.data.map((project) => {
         return project.name;
       });
       console.log(labels);
 
-      const counts = data.map((project) => {
+      const counts = response.data.map((project) => {
         return project.logs.length;
       });
       console.log(counts);
       setChartData({
-        labels: labels || [],
-
-        ...chartData,
+        labels: labels,
+        datasets: [
+          {
+            label: "time spent",
+            data: counts,
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(246, 158, 135,0.2)",
+              "rgba(246, 35, 135,0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(246, 158, 135,1)",
+              "rgba(246, 35, 135,1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
       });
-
-      console.log(chartData.labels);
-      console.log(chartData);
     });
-  }, []);
+  };
 
-  console.log(data);
+  useEffect(() => {
+    chart();
+  }, []);
 
   return (
     <div className="insights">
