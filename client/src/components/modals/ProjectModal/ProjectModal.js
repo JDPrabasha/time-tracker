d1d0ff07d3db85a395e42c6414d0ce294ddf289a
Dulaj <shad3s.of.m3@gmail.com>
2022-05-07 18:05:55 +0530

    📦 latest stable release

--- client/src/components/modals/ProjectModal/ProjectModal.js
+++ client/src/components/modals/ProjectModal/ProjectModal.js
@@ -3,27 +3,6 @@ import "./ProjectModal.scss";
 import { RiCloseLine } from "react-icons/ri";
 import { addProject } from "../../../services/api";
 
-const dropIn = {
-  hidden: {
-    y: "-100vh",
-    opacity: 0,
-  },
-  visible: {
-    y: "0",
-    opacity: 1,
-    transition: {
-      duration: 0.1,
-      type: "spring",
-      damping: 25,
-      stiffness: 400,
-    },
-  },
-  exit: {
-    y: "100vh",
-    opacity: 0,
-  },
-};
-
 function ProjectModal({ setIsOpen }) {
   const [projectValue, setprojectValue] = useState({
     name: "",
