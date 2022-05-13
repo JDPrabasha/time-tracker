3785b1ac0763505be6d522fe7819825872ef254c
Dulaj <shad3s.of.m3@gmail.com>
2022-05-13 17:31:28 +0530

    change log structure

--- client/src/components/Log/Log.js
+++ client/src/components/Log/Log.js
@@ -4,19 +4,18 @@ import "./Log.scss";
 function Log(props) {
   return (
     <div className="log border rounded-lg px-3 py-1 mb-4">
-      <div className="row">
-        <div className="log__time">
+      <div className="flex items-center">
+        <div className="log__entry">
+          <p className="log__entry__activity font-semibold">{props.entry}</p>
+          <h4 className="log__entry__project text-sm">{props.name}</h4>
+        </div>
+        <div className="log__time ml-auto mr-3">
           <a className="log__time__duration">
             <span>{props.beginTime}</span>
             <span> - </span>
             <span>{props.endTime}</span>
           </a>
           <p className="log__time__date">{props.date.split("T")[0]}</p>
-        </div>
-
-        <div className="log__entry">
-          <h4 className="log__entry__project">{props.name}</h4>
-          <p className="log__entry__activity">{props.entry}</p>
         </div>
       </div>
     </div>
