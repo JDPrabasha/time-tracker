0fcf32bf9440a1c8edecfad473c41ca6cc4e4997
YaiyaSam <kalsha.sam26@gmail.com>
2022-05-20 21:40:56 +0530

    Group the logs by their created date

--- client/src/containers/Logs/Logs.js
+++ client/src/containers/Logs/Logs.js
@@ -3,6 +3,31 @@ import { Log } from "../../components";
 import { MdFormatListBulleted } from "react-icons/md";
 import { getLogs } from "../../services/api";
 
+/*---------grouping the logs by their created date---------*/
+function groupBy(array, property) {
+  return array.reduce(function (newArray, obj) {
+    let today = new Date();
+    let date  = new Date(obj[property]);
+    //var days = ['MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY'];
+    //var months = ['JANUARY','FEBRUARY','MARCH','APRIL','MAY','JUNE','JULY','AUGUST','SEPTEMBER','OCTOBER','NOVEMBER','DECEMBER'];
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    var key = "";
+    if(date.toDateString() == today.toDateString()){
+      key = "Today";
+    }
+
+    /*--------get the indexes of the new array to be in "day, date month" format (Ex:Monday, 9 January)---------*/
+    else key = days[date.getDay()] + ", " + date.getDate() + " " + months[date.getMonth()];
+    //console.log(key);
+    if (!newArray[key]) {
+      newArray[key] = [];
+    }
+    newArray[key].push(obj);
+    return newArray;
+  }, []);
+}
+
 function Logs() {
   const [logs, setLogs] = useState([]);
 
