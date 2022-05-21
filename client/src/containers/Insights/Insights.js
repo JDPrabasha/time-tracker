925d738ae2f7a89b421860eab4e4f8dd20d1e346
YaiyaSam <kalsha.sam26@gmail.com>
2022-05-21 12:13:28 +0530

    Commit before merging

--- client/src/containers/Insights/Insights.js
+++ client/src/containers/Insights/Insights.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MdLightbulbOutline } from "react-icons/md";
 import { Chart } from "../../components";
 
